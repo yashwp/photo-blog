6bbe4f01c4db2fcca4776d0d049e8b58ed810b30
Yashwardhan Pauranik <pauranik51@gmail.com>
2019-12-09 00:05:47 +0530

    feat: Setting cookie

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	uuid "src/github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
@@ -18,5 +20,30 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	c := getCookie(w, req)
+	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+
+	if err != nil {
+		sID, err := uuid.NewV4()
+		handleError(err)
+
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
 }
